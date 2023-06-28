1f3d5802c79a1257cae071d2c7cc44926246f964
trond.stromlie@tietoevry.com <TI-bbllhdnu1>
2023-06-28 13:38:33 +0200

    fix tests

--- tests/isKidValid.spec.js
+++ tests/isKidValid.spec.js
@@ -1,16 +1,19 @@
-const isKidValidSpec = require('../src/isKidValid')
+const {isKidValid} = require('../src/isKidValid')
 
 describe('Test isValidKid', () => {
     test("It should return false on empty input", () => {
-        expect(isKidValidSpec("")).toBe(false)
+        expect(isKidValid("")).toBe(false)
     });
     test("It should return false on wrong input", () => {
-        expect(isKidValidSpec("321654sad")).toBe(false)
+        expect(isKidValid("321654sad")).toBe(false)
     });
     test("It should return false on not valid kid number", () => {
-        expect(isKidValidSpec("0114641212")).toBe(false)
+        expect(isKidValid("0114641212")).toBe(false)
     });
+    test("It should return an error if the input is not a string", () => {
+        expect(() => isKidValid(123)).toThrow("Parameter is not a string!")
+    })
     test("It should return true when input is a valid kid number", () => {
-        expect(isKidValidSpec("0114641202")).toBe(true)
+        expect(isKidValid("0114641202")).toBe(true)
     });
 })
